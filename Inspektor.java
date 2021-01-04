c552423b2b93e94acdfa3fdba536da264719a870
bhornerETHZ <76952487+bhornerETHZ@users.noreply.github.com>
2021-01-04 17:07:19 +0100

    Update Inspektor.java

--- Inspektor.java
+++ Inspektor.java
@@ -1,32 +1,24 @@
-// Exercise 4
+// Exercise 4: file 1/2
 package eprogFS19;
 
 import java.util.Map;
 
 public class Inspektor {
 
-	public static void main(String[] args) {
-		eintrag("Stall-1 0.5 0.1667 XYZ");
-		// gefahrenStufe(); // -> OK
-		// besteStaelle(); // -> {XYZ=Stall-1}
-		eintrag("Stall-2 -1");
-		// gefahrenStufe(); // -> PANDEMIE
-		eintrag("Stall-2 0.7 0.15 ABC");
-		eintrag("Stall-3 0.7 0.185 ABC");
-		gefahrenStufe(); // -> EPIDEMIE
-		// besteStaelle(); // -> {ABC=Stall-3, XYZ=Stall-1}
+	public Inspektor() {
+		// TODO: maybe (depending on implementation
 	}
 
-	public static void eintrag(String input) {
+	public void eintrag(String input) {
 		// TODO
 	}
 
-	public static String gefahrenStufe() {
+	public String gefahrenStufe() {
 		// TODO
 		return null;
 	}
 
-	public static Map besteStaelle() {
+	public Map besteStaelle() {
 		// TODO
 		return null;
 	}
