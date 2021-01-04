a3d027e1c2c30323afbda07606cc60881598d978
bhornerETHZ <76952487+bhornerETHZ@users.noreply.github.com>
2021-01-04 17:06:44 +0100

    Create runInspektor.java

---
+++ runInspektor.java
@@ -0,0 +1,20 @@
+// Exercise 4
+package eprogFS19;
+
+public class runInspektor {
+
+	public static void main(String[] args) {
+		Inspektor inspektor = new Inspektor();
+		
+		inspektor.eintrag("Stall-1 0.5 0.1667 XYZ");
+		// gefahrenStufe(); // -> OK
+		// besteStaelle(); // -> {XYZ=Stall-1}
+		inspektor.eintrag("Stall-2 -1");
+		// gefahrenStufe(); // -> PANDEMIE
+		inspektor.eintrag("Stall-2 0.7 0.15 ABC");
+		inspektor.eintrag("Stall-3 0.7 0.185 ABC");
+		inspektor.gefahrenStufe(); // -> EPIDEMIE
+		// besteStaelle(); // -> {ABC=Stall-3, XYZ=Stall-1}
+	}
+
+}
