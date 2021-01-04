92dc4d67d73fd60756f5ac0fbee36360c0b31ce9
bhornerETHZ <76952487+bhornerETHZ@users.noreply.github.com>
2021-01-04 14:26:02 +0100

    Create Matrix.java

---
+++ Matrix.java
@@ -0,0 +1,22 @@
+// Exercise 2
+package prüfungHs19;
+
+public class Matrix {
+	
+	public static void main(String[] args) {
+		char[][] m = new char[][]{	{'g','e','e','a','p','r','o','g','o'},
+									{'o','p','b','p','e','p','r','o','g'},
+									{'r','r','c','c','r','c','c','c','e'},
+									{'p','o','d','d','d','o','d','d','p'}, 
+									{'e','g','e','e','e','e','g','e','r'}
+								};
+		
+		Position[] result = findWord(m, "eprog");
+		//TODO
+	}
+	
+	public static Position[] findWord(char[][] matrix, String word) {
+		//TODO
+		return null;		
+	}
+}
