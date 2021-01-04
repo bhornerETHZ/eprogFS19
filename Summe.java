4032940112e4e011bef66a303bbe18bd28986570
bhornerETHZ <76952487+bhornerETHZ@users.noreply.github.com>
2021-01-04 14:18:20 +0100

    Create Summe.java

---
+++ Summe.java
@@ -0,0 +1,19 @@
+package eprogHs19;
+
+public class Summe {
+
+	public static void main(String[] args) {
+		try {
+			System.out.println(ohne7(6));
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public static Pair ohne7(int n) throws Exception {
+		// TODO
+		return null;
+	}
+
+}
+
