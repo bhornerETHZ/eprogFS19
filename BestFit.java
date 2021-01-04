55fda21c85177ee33fe24bcb263de7126df3f9b7
bhornerETHZ <76952487+bhornerETHZ@users.noreply.github.com>
2021-01-04 15:47:35 +0100

    Create BestFit.java

---
+++ BestFit.java
@@ -0,0 +1,17 @@
+// Exercise 3
+package eprogFS19;
+
+import java.util.ArrayList;
+
+public class BestFit {
+
+	public static void main(String[] args) {
+		int[] v = { 3, 4, 7 };
+		int a = 6;
+		System.out.println(bestFit(v, a));
+	}
+
+	public static ArrayList<?> bestFit(int[] values, int average) {
+		return null;
+	}
+}
