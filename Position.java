ff13d52209e64bc136d6412259962acbcc2b60f0
bhornerETHZ <76952487+bhornerETHZ@users.noreply.github.com>
2021-01-04 14:15:49 +0100

    Create Position.java

---
+++ Position.java
@@ -0,0 +1,14 @@
+package eprogHs19;
+
+public class Position {
+	int x;
+	int y;
+
+	public Position(int i, int j) {
+		this.x = i;
+		this.y = j;
+	}
+	public String toString() {
+		return "( " + x + ", " + y + ")";
+	}
+}
