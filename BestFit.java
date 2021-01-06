98c740ef5a1daa62040da99c7f87fc3f221b44d9
Lukas <Lukas@10.5.219.66>
2021-01-06 15:59:50 +0100

    neu

--- BestFit.java
+++ BestFit.java
@@ -4,13 +4,23 @@
 import java.util.*;
 public class BestFit {
 	public static void main(String[] args) {
-		HashSet<Integer> test = new HashSet<Integer>();
+		HashSet<Integer> test = new HashSet<Integer>();		//Example 1
 		test.add(3);
 		test.add(4);
 		test.add(7);
-		int D=6;
-		ArrayList<Integer> res = bestFit(test, D);
-		System.out.println(res);
+		int D1=6;
+		ArrayList<Integer> res1 = bestFit(test, D1);
+		System.out.println(res1);
+		
+		HashSet<Integer> test2 = new HashSet<Integer>();	//Example 2
+		test2.add(0);
+		test2.add(3);
+		test2.add(6);
+		test2.add(10);
+		int D2=5;
+		ArrayList<Integer> res2 = bestFit(test2, D2);
+		System.out.println(res2);
+		
 	}
 	
 	
