20f6a027e97f4517c27948a214907114816d8ea0
Lukas <Lukas@10.5.219.66>
2021-01-06 15:40:21 +0100

    Aufg. 3

---
+++ BestFit.java
@@ -0,0 +1,28 @@
+//Exercise 3
+
+
+import java.util.*;
+public class BestFit {
+	public static void main(String[] args) {
+		HashSet<Integer> test = new HashSet<Integer>();
+		test.add(3);
+		test.add(4);
+		test.add(7);
+		int D=6;
+		ArrayList<Integer> res = bestFit(test, D);
+		System.out.println(res);
+	}
+	
+	
+	
+	public static ArrayList<Integer> bestFit(Set<Integer> werte, int durchschnitt) {
+		//TODO
+		
+		return null;
+		
+		
+		
+		
+	}
+}
+
